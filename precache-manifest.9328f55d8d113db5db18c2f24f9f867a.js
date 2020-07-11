1fc04c8ed455a7cef1d966a0e7406f30e19d9edf
Tanjim Hossain <audacioustux@protonmail.com>
2020-07-11 23:10:40 +0600

    init

---
+++ precache-manifest.9328f55d8d113db5db18c2f24f9f867a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38a5f94553f058fa6c1c",
+    "url": "/css/app.9c909c41.css"
+  },
+  {
+    "revision": "e8c33d176ee72c431aa8",
+    "url": "/css/chunk-vendors.d8d5be88.css"
+  },
+  {
+    "revision": "1164664c73ab263443c94f4d1154ebf6",
+    "url": "/img/scaled.1164664c.webp"
+  },
+  {
+    "revision": "60008ee16029313462b2965e2d322320",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38a5f94553f058fa6c1c",
+    "url": "/js/app.bad28af5.js"
+  },
+  {
+    "revision": "e8c33d176ee72c431aa8",
+    "url": "/js/chunk-vendors.4cf0f075.js"
+  },
+  {
+    "revision": "c3227ab8cbcf508b7bf047d7c1d1543a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
